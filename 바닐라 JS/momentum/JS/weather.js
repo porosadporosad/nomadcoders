f08e2f8ef76a1c0f9eb22fc3be0a0a7cf1890314
porosadporosad <tmxk1594@naver.com>
2023-01-20 03:12:22 +0900

    바닐라JS

---
+++ 바닐라 JS/momentum/JS/weather.js
@@ -0,0 +1,22 @@
+
+const API_KEY = "943fcfad8d82fbee2254d8ea4233adbb";
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const weather = document.querySelector("#weather span:first-child");
+            const City = document.querySelector("#weather span:last-child");
+            City.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
