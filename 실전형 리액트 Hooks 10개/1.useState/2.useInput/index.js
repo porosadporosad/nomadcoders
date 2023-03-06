f3d3b9029acd86c01ddd1ed99dd371085d3f13fb
porosadporosad <tmxk1594@naver.com>
2023-03-06 16:57:29 +0900

    Hooks

---
+++ 실전형 리액트 Hooks 10개/1.useState/2.useInput/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+import "./stayles.css";
+
+const useInput = (initialValue, validator) => {
+    const [value, setValue] = useState(initialValue);
+    const onChange = event => {
+        const {
+            target: {value}
+        } = event;
+        let willUpdate = true;
+        if(typeof validator==="function"){
+            willUpdate = validator(value);
+        }
+        if(willUpdate){
+            setValue(value);
+        }  
+    };
+    return {value, onChange};
+};
+
+const App = () => {
+    const maxLen = value => !value.includes("@");
+    const name = useInput("Mr.", maxLen);
+    return (
+        <div className="App">
+            <h1>Hello</h1>
+            <input placeholder="Name" {...name} />
+        </div>
+    );
+};
+
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
