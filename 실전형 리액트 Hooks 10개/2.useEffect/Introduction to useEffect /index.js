d8baaa18b5828be8e228cee32d498516c193b077
porosadporosad <tmxk1594@naver.com>
2023-03-07 17:13:45 +0900

    2..

---
+++ 실전형 리액트 Hooks 10개/2.useEffect/Introduction to useEffect /index.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+    const sayHello = () => console.log("hello");
+    const [number, setNumber] = useState(0);
+    const [aNumber, setAnumber] = useState(0);
+    useEffect(sayHello,[number]);
+    return (
+        <div className="App">
+           <div>Hi</div>
+           <button onClick={()=> setNumber(number + 1)}>{number}</button>
+           <button onClick={()=> setAnumber(number + 1)}>{aNumber}</button>
+        </div>
+    );
+};
+
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
