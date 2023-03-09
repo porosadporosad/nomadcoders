c70e39d171d674bf8755d1eedf3fb5e4a6d87031
porosadporosad <tmxk1594@naver.com>
2023-03-09 21:12:25 +0900

    2

---
+++ 실전형 리액트 Hooks 10개/2.useEffect/useConfirm/index.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+
+const useConfirm = (message="", callback, rejection) => {
+    if(typeof callback !== "function"){
+        return;
+    }
+    const confirmAction = () => {
+        if(confirm(message)){
+            callback;
+        } else {
+            rejection();
+        }
+    };
+    return confirmAction;
+};
+
+const App = () => {
+    const deleteWorld = () => console.log("Deleteing the world.");
+    const abort = () => console.log("Aborted"); 
+    const confirmDelete = useConfirm("Are you sure", deleteWorld, abort);
+    return (
+        <div className="App">
+           <button onClick={confirmDelete}>Delete the world</button>
+        </div>
+    );
+};
+
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
