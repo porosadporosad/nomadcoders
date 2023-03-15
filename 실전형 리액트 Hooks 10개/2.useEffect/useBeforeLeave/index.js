5b4f59644294596dbfccd18b4432ea8c9d056a10
porosadporosad <tmxk1594@naver.com>
2023-03-15 17:41:24 +0900

    2

---
+++ 실전형 리액트 Hooks 10개/2.useEffect/useBeforeLeave/index.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+
+const useBeforeLeave = (onBefore) => {
+    if(typeof onBefore !== "function"){
+        return;
+    }
+    const handle = (event) => {
+        const { clientY } = event;
+        if(clientY <= 0 ){
+            onBefore();
+        }
+        
+    };
+    useEffect(() => {
+        document.addEventListener("mouseleave",handle);
+        return () => document.removeEventListener("mouseleave",handle);
+    },[]);
+};
+
+const App = () => {
+    const begForLife = () => console.log("Pls dont leave");
+    useBeforeLeave(begForLife);
+    return (
+        <div className="App">
+           <h1>Hello</h1>
+        </div>
+    );
+};
+
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
