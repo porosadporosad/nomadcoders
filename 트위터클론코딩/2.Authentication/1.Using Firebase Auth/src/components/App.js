17361ba9eebfbf36a2aaf9674033b2c6f673907f
porosadporosad <tmxk1594@naver.com>
2023-03-29 15:49:35 +0900

    2-1

---
+++ 트위터클론코딩/2.Authentication/1.Using Firebase Auth/src/components/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import AppRouter from 'components/Router';
+import { auth } from "fbase";
+
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(auth.currentUser);
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn}/>
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
+  );
+}
+
+export default App;
