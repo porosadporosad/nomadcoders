55a017bd4875a5340030252b2aa9a6e4a3d31fd5
porosadporosad <tmxk1594@naver.com>
2023-06-14 17:13:39 +0900

    5-3

---
+++ 트위터클론코딩/5. Edit Profile/3. Update Profile Bugfix/Profile.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import {authService} from "fbase";
+import { updateProfile } from "@firebase/auth";
+import {useHistory} from 'react-router-dom';
+
+export default ({refreshUser,userObj}) => {
+    const history = useHistory();
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+    const onLogOutClick = () => {
+        authService.signOut();
+        history.push("/");
+    }
+    const onChange = (event) => {
+        const {
+            target: {value},
+        } = event;
+        setNewDisplayName(value);
+    }
+    const onSubmit = async(event) => {
+        event.preventDefault();
+        if(userObj.displayName !== newDisplayName){
+            await updateProfile(authService.currentUser, { displayName: newDisplayName });
+            refreshUser();
+            }
+    };
+   
+    return (
+        <>
+        <form onSubmit={onSubmit}>
+            <input onChange={onChange} type="text" placeholder="Display name" value={newDisplayName} />
+            <input type="submit" value="Update Profile" />
+        </form>
+            <button onClick={onLogOutClick}>Log out</button>
+        </>
+    );
+}
